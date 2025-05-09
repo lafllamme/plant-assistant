8d8ad586301f6d0bab7a8b7efcb088d65c5a90e3
Lafllamme <teke900@gmail.com>
2025-05-09 21:48:58 +0200

    feat: Add mobile home page with Ionic and Nuxt integration
    Introduce a new mobile home page leveraging Ionic components in a Nuxt framework. The page includes a header, content area, and a dynamic value displayed using `useFoo()`. This serves as the foundation for future mobile-specific features.

---
+++ apps/mobile/pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+// ← this comes for free via auto-import!
+const foo = useFoo()
+
+consola.log('foo', foo)
+</script>
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Mobile Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      Hello from Ionic + Nuxt! {{ foo }}
+    </ion-content>
+  </ion-page>
+</template>
